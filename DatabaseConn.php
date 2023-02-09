23785fcd151ac494a9939a79e5b6a401238b3cd4
pablo <pablo@gmail.com>
2023-02-09 14:09:44 +0100

    afddee

--- DatabaseConn.php
+++ DatabaseConn.php
@@ -2,29 +2,36 @@
 
 class DatabaseConn
 {
-    const servername = "localhost";
-    const username = "forti";
-    const password = "forti";
-    const dbname = "forti";
-
-    // Create connection
-    const conn = new mysqli(servername, username, password, dbname);
+    protected $conn;
 
 
+    function __construct()
+    {
+        $servername = "localhost";
+        $username = "forti";
+        $password = "forti";
+        $dbname = "forti";
+
+        // Create connection
+        $this->conn = new mysqli($servername, $username, $password, $dbname);
+    }
 
     function comprobarLogin($username, $password)
     {
 
         try {
-            $sql = "SELECT * FROM Usuario WHERE username=" . $username . " AND password= " . $password;
-            $result = self::conn->query($sql);
+            $sql = "SELECT * FROM Usuario u WHERE u.username='$username' AND u.password='$password'";
+            
+            $result = $this->conn->query($sql);
+            echo "\n" . $username . "\n" . $password . "\n";
+            echo $result->fetch_assoc();
         } catch (Exception $e) {
             echo $e;
         }
         if ($result->num_rows > 0) {
-            return false;
+            return true;
         }
-        return true;
+        return false;
     }
     function comprobarRegistro($datos)
     {
@@ -33,14 +40,15 @@ class DatabaseConn
         $username = $datos["username"];
         $password = $datos["password"];
         $nombre = $datos["nombre"];
+        echo 'aqui';
 
         $problemaMail = false;
         $problemaUser = false;
 
 
         try {
-            $sql = "SELECT * FROM Usuario WHERE username=" . $username;
-            $result = self::conn->query($sql);
+            $sql = "SELECT * FROM Usuario WHERE username='$username'";
+            $result = $this->conn->query($sql);
         } catch (Exception $e) {
             echo $e;
         }
@@ -49,8 +57,8 @@ class DatabaseConn
         }
 
         try {
-            $sql = "SELECT * FROM Usuario WHERE email=" . $email;
-            $result = self::conn->query($sql);
+            $sql = "SELECT * FROM Usuario WHERE email='$email'";
+            $result = $this->conn->query($sql);
         } catch (Exception $e) {
             echo $e;
         }
@@ -75,7 +83,7 @@ class DatabaseConn
         $sql = "INSERT INTO Usuario VALUES($id, '$nombre', '$username', '$password', '$email')";
 
 
-        if (self::conn->query($sql) === TRUE) {
+        if ($this->conn->query($sql) === TRUE) {
             echo "New record created successfully";
         } else {
             return false;
@@ -89,10 +97,10 @@ class DatabaseConn
         $existeID = false;
         do {
             $existeID = false;
-            $id = random_int(0, PHP_INT_MAX);
+            $id = random_int(0, 2147483646);
             try {
                 $sql = "SELECT * FROM Usuario WHERE id=" . $id;
-                $result = self::conn->query($sql);
+                $result = $this->conn->query($sql);
             } catch (Exception $e) {
                 echo $e;
             }
--- controladorInicio.php
+++ controladorInicio.php
@@ -3,34 +3,47 @@
 require_once("DatabaseConn.php");
 
 
+try{
 
-if (isset($_POST["login"])) {
-    iniciarSesion();
+    if (isset($_POST["login"])) {
+        iniciarSesion();
+    }
+    if (isset($_POST["registro"])) {
+        echo 'aqui';
+        registrar();
+    }
+    echo 'nada';
 }
-if (isset($_POST["registro"])) {
-    registrar();
+catch(Exception $e){
+    echo 'sss';
 }
 
 function registrar()
 {
-    $database = new DatabaseConn();
-    $email = $_POST["email"];
-    $foto = $_FILES["foto"];
-    $username = $_POST["username"];
-    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
-    $nombre = $_POST["nombre"];
-    $res = $database->comprobarRegistro(["email" => $email, "username" => $username, "nombre" => $nombre, "password" => $password]);
-    if($res == "mail"){
-        echo "problemaMail";
+    try {
+        echo 'primero';
+
+
+        $database = new DatabaseConn();
+        echo 'segundo';
+        $email = $_POST["email"];
+        $foto = $_FILES["foto"];
+        $username = $_POST["username"];
+        $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+        $nombre = $_POST["nombre"];
+        $res = $database->comprobarRegistro(["email" => $email, "username" => $username, "nombre" => $nombre, "password" => $password]);
+        if ($res === "mail") {
+            echo "problemaMail";
+        } else if ($res === "user") {
+            echo "problemaUser";
+        } else if ($res) {
+            echo 'registrado';
+        } else {
+            echo 'fallo';
+        }
     }
-    else if($res == "user"){
-        echo "problemaUser";
-    }
-    else if($res){
-        echo 'registrado';
-    }
-    else{
-        echo 'fallo';
+    catch(Exception $e){
+        echo $e;
     }
 }
 
